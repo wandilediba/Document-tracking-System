631d183b9aca6e226268668e0961f2767a30fc4f
wandile62@gmail.com <wandile62@gmail.com@1acc18db-e345-81b9-89a1-0b64f38244b1>
2013-05-29 21:20:00 +0000

    Must do services and Test

---
+++ document_tracking_system/src/java/com/wandile/documenttracking/services/crud/EmployeeCrudModelServiceImpl.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wandile.documenttracking.services.crud;
+
+/**
+ *
+ * @author 209043946
+ */
+public interface EmployeeCrudModelServiceImpl {
+    
+}
