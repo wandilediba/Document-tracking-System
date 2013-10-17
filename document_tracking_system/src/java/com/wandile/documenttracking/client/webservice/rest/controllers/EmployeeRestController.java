51a162d8dfd28008640280a86ec84b1b676abf17
Wandile62 <wandile62@gmail.com>
2013-10-17 13:31:02 +0000

    persisting to database and showing data on the web

--- document_tracking_system/src/java/com/wandile/documenttracking/client/webservice/rest/controllers/EmployeeRestController.java
+++ document_tracking_system/src/java/com/wandile/documenttracking/client/webservice/rest/controllers/EmployeeRestController.java
@@ -6,10 +6,12 @@ package com.wandile.documenttracking.client.webservice.rest.controllers;
 
 import com.wandile.documenttracking.client.webservice.rest.resources.EmployeeResources;
 import com.wandile.documenttracking.domain.Employee;
+import com.wandile.documenttracking.services.EmployeeService;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,39 +24,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
  @RequestMapping(value = "employee", method = RequestMethod.GET)
 public class EmployeeRestController {
     
-   // public  List<EmployeeResources> getEmployee(){
+   private EmployeeService employeeService;
+   @ResponseBody
+    @RequestMapping( value="employee", method = RequestMethod.GET)
+    public List<EmployeeResources> getCourses(Model model){
+        List<Employee> employees = employeeService.getEmployee();
         
-    /*List<Employee> employee =  employeeService.findAll();
-    
-    List<EmployeeResources> rs = (List<EmployeeResources>) new ArrayList<EmployeeResources>();
-    
-    for(Employee employee: employees){
-       List<EmployeeResources> emp = new ArrayList< EmployeeResources>();
-       emp.setEmployeeName(employee.getEmployeeName());
-       emp.setContact(employee.getContact());
-       emp.setDepartment(employee.)
-       rs.add(emp);   
-       }
-       
-       return rs;
-       
-       /* @ResponseBody
-    @RequestMapping(value = "tournaments", method = RequestMethod.GET)   
-    public List<TournamentResource> getTournaments(){
-        return null;
-        /*List<Tournament> tournaments = tournamentService.findAll();
-        List<TournamentResource> tr = new ArrayList<TournamentResource>();
-        for(Tournament tournament:tournaments){
-            TournamentResource t = new TournamentResource();
-            t.setName(tournament.getName());
-            t.setTournamentNumber(tournament.getTournamentNumber());
-            t.setVenue(tournament.getVenue());
-            t.setSponsor(tournament.getSponsor());
-            t.setId(tournament.getId());
-            t.setResults(tournament.getResults());
-            t.setReferee(tournament.getReferee());
-            tr.add(t);
-        }*/
+         List<EmployeeResources> rs = new ArrayList<EmployeeResources>();
+         System.out.println(" THE EMPLOYEE SIZE "+employees.size());
+        for (Employee employee : employees) {
+            EmployeeResources c = new EmployeeResources();
+            c.setContact(employee.getContact());
+            c.setEmployeenumber(employee.getEmployeeNumber());
+            c.setName(employee.getName());
+           
+            rs.add(c);
+        }
+       return rs; 
+    }
        
     
      }
